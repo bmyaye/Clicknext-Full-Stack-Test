da9e9a4d936d14fadb758334ab2ac2d3b28594d2
Ai Mousika <prxvatex@gmail.com>
2024-07-26 21:26:09 +0700

    commit no.1

--- no1.py
+++ no1.py
@@ -1,1 +1,16 @@
-print("Hello, World!")
+# no.1 sum array
+
+a = input("Enter the array: ").split()
+a = [int(i) for i in a]
+
+s = int(input("Enter the sum: "))
+
+result = []
+
+for i in range(len(a)):
+    for j in range(i + 1, len(a)):
+        if a[i] + a[j] == s:
+            result.append([a[i], a[j]])
+
+for i in result:
+    print(i)
