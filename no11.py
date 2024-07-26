858175ceae58490f687c0d84275895b35e980431
Ai Mousika <prxvatex@gmail.com>
2024-07-26 23:20:17 +0700

    commit no.11

---
+++ no11.py
@@ -0,0 +1,15 @@
+# no.11 sort int
+
+# a = input("Enter the number: ").split()
+a = input("Enter the number: ").split(", ")
+a = [int(i) for i in a]
+
+for i in range(len(a)):
+    for j in range(0, len(a)-i-1):
+        if a[j] > a[j+1]:
+            a[j], a[j+1] = a[j+1], a[j]
+
+result = [str(i) for i in a]
+print(', '.join(result))
+
+# print(a)
